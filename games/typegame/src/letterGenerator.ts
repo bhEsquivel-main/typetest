455f00c647593554839f57fddda6bef97617e6a1
bhEsquivel-sw <bernard.esquivel@mnltechnology.com>
2022-07-15 16:28:51 +0800

    removed space on alphabet

--- games/typegame/src/letterGenerator.ts
+++ games/typegame/src/letterGenerator.ts
@@ -6,7 +6,7 @@ export class LetterGenerator
 {
 
     private static alphabet: string = "a,b,b,b,b,b,b,b,c,c,c,c,c,c,c,d,d,d,d,d,d,e," +
-                                    "f,f,,f,f,f,f,g,g,g,g,g,h,h,,h,h,h,h,"+
+                                    "f,f,f,f,f,f,g,g,g,g,g,h,h,h,h,h,h,"+
                                     "i,j,j,j,j,j,j,k,k,l,l,l,l,m,m,m,m,n,n,n,o,"+
                                     "p,p,q,q,q,r,r,s,s,s,t,t,u,"+
                                     "v,v,v,v,w,w,x,x,y,y,z,z,z,z";
